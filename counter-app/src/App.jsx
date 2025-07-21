5539f5622d91cadb0a83626462e1656faef374fb
Kirk Kudoto <135407806+Kirk-kud@users.noreply.github.com>
2025-07-21 14:51:04 +0000

    Deploying the first version of the counter-app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -4,30 +4,79 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ count, setCount ] = useState(0)
+  const [ min, setMin ] = useState(0);
+  const [ max, setMax ] = useState(15);
+
+  const decreaseCount = () => {
+    console.log("Count: " + count + " Min: " + min);
+    if (count === min || count < min){
+     setCount(count);
+     document.getElementById('error-detail').innerText = "Count has reached its minimum: " + min;
+   }
+   else{
+     setCount(count - 1);
+     console.log("Count decremented by 1");
+     document.getElementById('error-detail').innerText = "";
+   }
+  };
+
+  const increaseCount = () => {
+    console.log("Count: " + count + " Max: " + max);
+    if (count === max || count > max){
+     setCount(count);
+     document.getElementById('error-detail').innerText = "Count has reached its max: " + max;
+   }
+   else{
+     setCount(count + 1);
+     console.log("Count incremented by 1");
+     document.getElementById('error-detail').innerText = "";
+   }
+  }
+
+  const handleMinChange = (e) => {
+    setMin(parseInt(e.target.value));
+    setCount(min);
+  };
+
+  const handleMaxChange = (e) => {
+    setMax(parseInt(e.target.value));
+    setCount(max);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Welcome to the Counter App</h1>
+        <h2>Count: {count}</h2>
+        <div>
+          <label>
+            Set Minimum Value:
+          </label>
+          <input onChange={handleMinChange} type="number" value={min}/>
+          <label>
+            Set Maximum Value:
+          </label>
+          <input onChange={handleMaxChange} type="number" value={max}/>
+        </div>
+        <div>
+          <label>
+            Increase:
+          </label>
+          <button onClick={increaseCount}>
+            +
+          </button>
+          <label>
+            Decrease:
+          </label>
+          <button onClick={decreaseCount}>
+            -
+          </button>
+        </div>
+        <h2 id="error-detail">
+
+        </h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
